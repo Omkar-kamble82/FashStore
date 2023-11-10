00bd6576907ebf96b8c7a995955ec063db5bd778
Omkar-kamble82 <omkarsk82@gmail.com>
2023-11-10 12:14:35 +0530

    final

--- client/src/404.tsx
+++ client/src/404.tsx
@@ -9,7 +9,7 @@ const Notfound = () => {
                 <div className="h-[60vh] flex justify-center items-center flex-col">
                     <img className="w-[300px] sm:w-[450px]" src="/404.png" alt="404-img"/>
                     <p className="text-[50px] font font-bold text-[#e7ab3c]">Page Not Found</p>
-                    <a href="/home"><button className={`my-2 w-[260px] px-10 py-3 flex justify-center shadow-lg font-semibold text-md text-white bg-[#e7ab3c] rounded-full focus:outline-none  hover:shadow-md z-10`}>Go to home</button></a>
+                    <a href="/home"><button className={`my-2 w-[260px] px-10 py-3 flex justify-center shadow-lg font-semibold text-md text-white bg-[#e7ab3c] rounded-full focus:outline-none  hover:shadow-md z-10`}>Go Back to homepage</button></a>
                 </div>
                 <Footer />
             </div>
